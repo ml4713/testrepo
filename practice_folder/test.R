b8433858c55e1638ab19b5f328593dd7144df8c7
Rita Li <ml4713@nyu.edu>
2017-09-21 15:37:37 -0400

    bug fixed

--- practice_folder/test.R
+++ practice_folder/test.R
@@ -1,5 +1,13 @@
 #this is a test for R
 library(data.table)
-library(randomForests)
+library(lubridate)
 
 print ("this part had completed and libraries are loaded!!")
+
+data(iris)
+head(iris)
+
+
+summary(iris)
+
+
